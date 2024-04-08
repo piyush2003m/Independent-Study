d026e33538a0f91aa4749f2117898619bc7f4421
piyush2003m <piyush2003m@gmail.com>
2024-04-08 08:34:16 -0400

    all facet

--- trec-eval/makeFiles.py
+++ trec-eval/makeFiles.py
@@ -77,24 +77,47 @@ tsv file where every line corresponds to a query document pair and its relevance
 
 
 def createGoldFile(facet):
-    filePath = "../gold/test-pid2anns-csfcube-" + facet + ".json"
-    # split = "test"
-    testFolds = ["1", "2"]
-    for test in testFolds:
-        qrels_file_path = f"./GoldFiles/{facet}/csfcube-{facet}-fold{test}_test.qrels"
-        # newline to prevent extra newline characters and currently using qrel filename
-        with codecs.open(filePath, 'r', 'utf-8') as fp, open(qrels_file_path, 'w', newline='') as tsv_file:
-            qpid2pool = json.load(fp)
-            key = f"fold{test}_test"
-            testQPids = facet2folds[facet][key]
+    if facet == "all":
+        for subFacet in ["background", "method", "result"]:
+            filePath = "../gold/test-pid2anns-csfcube-" + subFacet + ".json"  
+            testFolds = ["1", "2"]
+            for test in testFolds:
+                qrels_file_path = f"./GoldFiles/{facet}/csfcube-{facet}-fold{test}_test.qrels"
+                with codecs.open(filePath, 'r', 'utf-8') as fp, open(qrels_file_path, 'w', newline='') as tsv_file:
+                    # load gold json of sub-facet
+                    qpid2pool = json.load(fp)
+                    key = f"fold{test}_test"
+                    # query documents from facet2folds
+                    testQPids = facet2folds[facet][key]
+                    
+                    # loop over each query document in the fold
+                    for qpid in testQPids:
+                        # if query document facet is the same as the subFacet
+                        if subFacet in qpid:
+                            qpid = qpid.replace(f"_{subFacet}", "")
+                            # loop over each qpid' candidates in the gold file
+                            for i in range(len(qpid2pool[qpid]['cands'])):
+                                line = f"{qpid}\t0\t{qpid2pool[qpid]['cands'][i]}\t{qpid2pool[qpid]['relevance_adju'][i]}\n"
+                                tsv_file.write(line)
+    else:
+        filePath = "../gold/test-pid2anns-csfcube-" + facet + ".json"
+        # split = "test"
+        testFolds = ["1", "2"]
+        for test in testFolds:
+            qrels_file_path = f"./GoldFiles/{facet}/csfcube-{facet}-fold{test}_test.qrels"
+            # newline to prevent extra newline characters and currently using qrel filename
+            with codecs.open(filePath, 'r', 'utf-8') as fp, open(qrels_file_path, 'w', newline='') as tsv_file:
+                qpid2pool = json.load(fp)
+                key = f"fold{test}_test"
+                testQPids = facet2folds[facet][key]
 
-            for qpid in testQPids:
-                qpid = qpid.replace("_background", "")
-                qpid = qpid.replace("_method", "")
-                qpid = qpid.replace("_result", "")
-                for i in range(len(qpid2pool[qpid]['cands'])):
-                    line = f"{qpid}\t0\t{qpid2pool[qpid]['cands'][i]}\t{qpid2pool[qpid]['relevance_adju'][i]}\n"
-                    tsv_file.write(line)
+                for qpid in testQPids:
+                    qpid = qpid.replace("_background", "")
+                    qpid = qpid.replace("_method", "")
+                    qpid = qpid.replace("_result", "")
+                    for i in range(len(qpid2pool[qpid]['cands'])):
+                        line = f"{qpid}\t0\t{qpid2pool[qpid]['cands'][i]}\t{qpid2pool[qpid]['relevance_adju'][i]}\n"
+                        tsv_file.write(line)
 '''
 The predictions file needs to be tsv file also. Im sending you a function to write this out. 
 The format of the output is: qid, candid, rank positon, similarity, model_name. 
@@ -103,29 +126,50 @@ You can convert a L2 similarity to a distance as explained here:
 https://stats.stackexchange.com/q/53068/55807 – 1/(1 + L2 distance)
 '''
 def createRankedFile(facet, model):
-    filePath = f"../ranked/{model}/test-pid2pool-csfcube-{model}-{facet}-ranked.json"
-    testFolds = ["1", "2"]
-    for test in testFolds:
-        rankedFileName = f"./PredictionFiles/{facet}/test-pid2anns-csfcube-{facet}-fold{test}_test.tsv"
-        with codecs.open(filePath, 'r', 'utf-8') as fp, open(rankedFileName, 'w', newline='') as tsv_file:
-            qpid2pool = json.load(fp)
-            key = f"fold{test}_test"
-            testQPids = facet2folds[facet][key]
+    if facet == "all":
+        for subFacet in ["background", "method", "result"]:
+            filePath = f"../ranked/{model}/test-pid2pool-csfcube-{model}-{subFacet}-ranked.json"
+            testFolds = ["1", "2"]
+            for test in testFolds:
+                rankedFileName = f"./PredictionFiles/{facet}/test-pid2anns-csfcube-{facet}-fold{test}_test.tsv"
+                with codecs.open(filePath, 'r', 'utf-8') as fp, open(rankedFileName, 'w', newline='') as tsv_file:
+                    # load ranked json of sub-facet
+                    qpid2pool = json.load(fp)
+                    key = f"fold{test}_test"
+                    # query documents from facet2folds
+                    testQPids = facet2folds[facet][key]
 
-            for qpid in testQPids:
-                qpid = qpid.replace("_background", "")
-                qpid = qpid.replace("_method", "")
-                qpid = qpid.replace("_result", "")
-                for i in range(len(qpid2pool[qpid])):
-                    similarity = 1 / (1 + qpid2pool[qpid][i][1])
-                    line = f"{qpid}\tQ0\t{qpid2pool[qpid][i][0]}\t{i+1}\t{similarity}\t{model}\n"
-                    tsv_file.write(line)
+                    # loop over each query document in the fold
+                    for qpid in testQPids:
+                        # if query document facet is the same as the subFacet
+                        if subFacet in qpid:
+                            qpid = qpid.replace(f"_{subFacet}", "")
+                            # loop over each qpid' candidates in the ranked file
+                            for i in range(len(qpid2pool[qpid])):
+                                similarity = 1 / (1 + qpid2pool[qpid][i][1])
+                                line = f"{qpid}\tQ0\t{qpid2pool[qpid][i][0]}\t{i+1}\t{similarity}\t{model}\n"
+                                tsv_file.write(line)
+    else:
+        filePath = f"../ranked/{model}/test-pid2pool-csfcube-{model}-{facet}-ranked.json"
+        testFolds = ["1", "2"]
+        for test in testFolds:
+            rankedFileName = f"./PredictionFiles/{facet}/test-pid2anns-csfcube-{facet}-fold{test}_test.tsv"
+            with codecs.open(filePath, 'r', 'utf-8') as fp, open(rankedFileName, 'w', newline='') as tsv_file:
+                qpid2pool = json.load(fp)
+                key = f"fold{test}_test"
+                testQPids = facet2folds[facet][key]
+
+                for qpid in testQPids:
+                    qpid = qpid.replace("_background", "")
+                    qpid = qpid.replace("_method", "")
+                    qpid = qpid.replace("_result", "")
+                    for i in range(len(qpid2pool[qpid])):
+                        similarity = 1 / (1 + qpid2pool[qpid][i][1])
+                        line = f"{qpid}\tQ0\t{qpid2pool[qpid][i][0]}\t{i+1}\t{similarity}\t{model}\n"
+                        tsv_file.write(line)
     
 
-facets = ["background", "result", "method"] # , "all"
+facets = ["background", "result", "method", "all"] # , "all"
 for facet in facets:
     createGoldFile(facet)
     createRankedFile(facet, "specter")
-    # createRankedFile(facet, "specter")
-# createGoldFile("result")
-# createRankedFile("background", "specter")
